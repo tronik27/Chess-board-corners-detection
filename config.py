126d8349a6434f4868f8f2dcd96195b3b22e2536
Nikita <80170090+tronik27@users.noreply.github.com>
2021-10-07 22:30:22 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,21 +1,19 @@
 import os
 
 # Paths to data.
-# PATH = 'path/to/folder/containing/dataset'
-PATH_TO_IMAGES = r'E:\DATASETS\idchess_zadanie\xtrain.npy'
-PATH_TO_KEYPOINTS = r'E:\DATASETS\idchess_zadanie\ytrain.npy'
+PATH_TO_IMAGES = 'path/to/numpy array/containing/images'
+PATH_TO_KEYPOINTS = 'path/to/numpy array/containing/keypoints'
 PATH_TO_TRAIN_IMAGES = os.path.join(os.path.dirname(PATH_TO_IMAGES), 'train_images.npy')
 PATH_TO_VAL_IMAGES = os.path.join(os.path.dirname(PATH_TO_IMAGES), 'val_images.npy')
 PATH_TO_TRAIN_KEYPOINTS = os.path.join(os.path.dirname(PATH_TO_KEYPOINTS), 'train_keypoints.npy')
 PATH_TO_VAL_KEYPOINTS = os.path.join(os.path.dirname(PATH_TO_KEYPOINTS), 'val_keypoints.npy')
-# WORK_DATA_PATH = 'path/to/folder/containing/images/for/classification'
-WORK_DATA_PATH = r'E:\DATASETS\idchess_zadanie\xtest.npy'
+WORK_DATA_PATH = 'path/to/numpy array/containing/test images'
 VALID_SIZE = 0.3
 
 # Training parameters.
 BATCH_SIZE = 16
 LEARNING_RATE = 0.001
-NUM_EPOCHS = 135
+NUM_EPOCHS = 100
 
 # Custom model parameters.
 NUM_FILTERS = 4
@@ -31,10 +29,9 @@ WEIGHTS_PATH = 'keypoints_detection_model/weights_2'
 MODEL_PATH = '{} (trained_model2)'.format(MODEL_NAME)
 
 # augmentation configuration
-# AUG_CONFIG = ['vertical_flip', 'horizontal_flip', 'sharpen', 'blur']
-AUG_CONFIG = ['vertical_flip', 'horizontal_flip', 'blur']
+AUG_CONFIG = ['vertical_flip', 'horizontal_flip', 'sharpen', 'blur']
 
 # service parameters
 SHOW_LEARNING_CURVES = True
 SHOW_IMAGE_DATA = True
-NUM_OF_EXAMPLES = 10
+NUM_OF_EXAMPLES = 1
